50daf7dfb83af0ed679a5a855a469898976dc775
dlhardi <derricklhardison@gmail.com>
2021-01-20 18:34:08 -0500

    added seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT into department (name)
+VALUES("Sales"), ("Engineering"), ("Finance");
+
+INSERT into role (title, salary, department_id)
+VALUES
+("Sales Lead", 120000, 1),
+("Salesperson", 80000, 1),
+("Lead Engineer", 160000, 2),
+("Software Engineer", 95000, 2),
+("Accountant", 85000, 3);
