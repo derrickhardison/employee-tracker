34997824000c13502cddc9e6091dfa6bb32a6579
dlhardi <derricklhardison@gmail.com>
2021-01-21 15:22:32 -0500

    Added init function housing inquirer questions to begin application. Added viewEmployees function.

--- index.js
+++ index.js
@@ -16,3 +16,79 @@ connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
 });
+
+function init() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee roles",
+        "Done",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add department":
+          console.log("Add department was selected");
+          init();
+          break;
+
+        case "Add role":
+          console.log("Add role was selected");
+          init();
+          break;
+
+        case "Add employee":
+          console.log("Add employee was selected");
+          init();
+          break;
+
+        case "View departments":
+          console.log("View department was selected");
+          init();
+          break;
+
+        case "View roles":
+          console.log("View roles was selected");
+          init();
+          break;
+
+        case "View employees":
+          console.log("View employees was selected");
+          viewEmployees();
+          init();
+          break;
+
+        case "Update employee roles":
+          console.log("Update employee roles was selected");
+          init();
+          break;
+
+        case "Done":
+          console.log("Done was selected");
+          break;
+      }
+    });
+}
+
+// FUNCTION DEFINITIONS
+
+function viewEmployees() {
+    var query = "SELECT * FROM employee;";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+
+});
+}
+
+// FUNCTION CALLS
+init();
